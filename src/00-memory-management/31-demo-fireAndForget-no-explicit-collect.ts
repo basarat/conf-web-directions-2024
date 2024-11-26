e83fdc67b5ad0cb3c93bbff2a81e63a6c5446381
Basarat Ali Syed <basaratali@gmail.com>
2024-11-26 12:32:29 +1100

    easy diff

--- src/00-memory-management/31-demo-fireAndForget-no-explicit-collect.ts
+++ src/00-memory-management/31-demo-fireAndForget-no-explicit-collect.ts
@@ -1,7 +1,6 @@
 import { memoryUsed } from "./00-memoryUsed";
 import { consume } from "./20-consumeMemory";
 
-
 console.log("start", memoryUsed());
 consume("🗑️");
 console.log("end", memoryUsed());
